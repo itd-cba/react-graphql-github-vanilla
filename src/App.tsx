5f8d1a9f191b11519902a1426e7880061ea5b756
Carsten Bartelt <87694273+itd-cba@users.noreply.github.com>
2021-10-13 17:45:37 +0200

    ✨ add basic input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import axios from "axios";
 
 const TITLE = "React GraphQL Github Client";
@@ -10,9 +10,42 @@ const axiosGitHubGraphQL = axios.create({
 });
 
 function App() {
+  const [path, setPath] = useState(
+    "the-road-to-learn-react/the-road-to-learn-react"
+  );
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setPath(event.target.value);
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // fetch data
+  };
+
+  useEffect(() => {
+    const fetchFromGit = async () => {
+      await axiosGitHubGraphQL.get(`/${path}`);
+    };
+    fetchFromGit();
+  }, []);
+
   return (
     <div>
       <h1>{TITLE}</h1>
+
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">Show opem issues for https://github.com/</label>
+        <input
+          id="url"
+          value={path}
+          type={"text"}
+          onChange={onChange}
+          style={{ width: "300px" }}
+        />
+        <button type={"submit"}>Search</button>
+      </form>
+      <hr />
     </div>
   );
 }
